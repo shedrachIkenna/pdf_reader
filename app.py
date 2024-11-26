8056ca9a0999b083071ef155d982a6d62665a893
ShedrachIkenna <Shedrach686@gmail.com>
2024-11-26 05:25:43 +0100

    read and split pdf text into chunks

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st 
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -9,6 +10,17 @@ def get_pdf_text(pdf_docs):
         for page in pdf_reader.pages:
             text += page.extract_text()
     return text
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len,
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
 
 def main():
     load_dotenv()
@@ -28,7 +40,8 @@ def main():
                 raw_text = get_pdf_text(pdf_docs)
 
                 # Split text into chunks 
-
+                text_chunks = get_text_chunks(raw_text)
+                st.write(text_chunks)
 
                 # Create vector store with the embeddings 
 
